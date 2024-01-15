18a52e23787bc22d6dbd33c10e03fc05245e7e71
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-01-15 00:05:37 +0000

    Reassign 'vowels' variable to use a string of vowels for simplicity. Added comments for logic explanation

--- python/8_kyu/replace_all_vowels_with_exclamation_mark_in_the_sentence/solution.py
+++ python/8_kyu/replace_all_vowels_with_exclamation_mark_in_the_sentence/solution.py
@@ -1,3 +1,7 @@
 def replace_exclamation(st):
-    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    # vowels stored in a variable
+    vowels = "aeiouAEIOU"
+    # use map to iterate over each character in the given string
+    # use lambda function, replacing character with "!" if found in vowels
+    # join resulting elements back into a string
     return ("").join(map(lambda c: "!" if c in vowels else c, st))
