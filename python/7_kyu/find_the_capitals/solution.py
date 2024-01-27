09291d996e22088d5a10a798912cd6470b838aa4
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-01-27 22:44:50 +0000

    Refactored using list comprehension to reach a more concise solution

--- python/7_kyu/find_the_capitals/solution.py
+++ python/7_kyu/find_the_capitals/solution.py
@@ -1,8 +1,12 @@
 def capitals(word):
-    result = []
-    # use enumerate to access index and value of each character in the given word
-    for index, value in enumerate(word):
-        # if the value in the current iteration is uppercase, add to the result list
-        if value.isupper():
-            result.append(index)
-    return result
+    # First solution below:
+    # result = []
+    # for index, value in enumerate(word):
+    #     if value.isupper():
+    #         result.append(index)
+    # return result
+
+    # Refactored solution using list comprehension to create a new list
+    # use enumerate to access the index and value of each character in the given word
+    # if the character in the current iteration is uppercase, add its index to the resulting list
+    return [index for index, value in enumerate(word) if value.isupper()]
