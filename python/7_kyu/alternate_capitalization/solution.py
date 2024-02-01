855ebda8976afc57dc7d084c07a55ba0e298c174
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-02-01 23:26:01 +0000

    Refactored to a more performant solution using list comprehensions and join

--- python/7_kyu/alternate_capitalization/solution.py
+++ python/7_kyu/alternate_capitalization/solution.py
@@ -1,13 +1,21 @@
 def capitalize(s):
-    pattern_one = ""
-    pattern_two = ""
+    # first solution below
+        # pattern_one = ""
+        # pattern_two = ""
+        # for index, value in enumerate(s):
+        #     if index % 2 == 0:
+        #         pattern_one += f"{value.upper()}"
+        #         pattern_two += f"{value.lower()}"
+        #     else:
+        #         pattern_one += f"{value.lower()}"
+        #         pattern_two += f"{value.upper()}"
+        # return [pattern_one, pattern_two]
     
-    for index, value in enumerate(s):
-        if index % 2 == 0:
-            pattern_one += f"{value.upper()}"
-            pattern_two += f"{value.lower()}"
-        else:
-            pattern_one += f"{value.lower()}"
-            pattern_two += f"{value.upper()}"
+    
+    # refactored to a more performant solution
+    # use list comprehension to build the pattern and join, rather than a new string being created with each concatenation
+
+    pattern_one = ("").join([value.upper() if index % 2 == 0 else value.lower() for index, value in enumerate(s)])
+    pattern_two = ("").join([value.lower() if index % 2 == 0 else value.upper() for index, value in enumerate(s)])
     
     return [pattern_one, pattern_two]
