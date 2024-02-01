a55f4e74ead5bcb9485e63d81ce658c3198aaaea
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-02-01 22:12:26 +0000

    Updated first solution with learning of more concise syntax, although still less performant than the refactored solution using dictionaries

--- python/7_kyu/anagram_detection/solution.py
+++ python/7_kyu/anagram_detection/solution.py
@@ -3,9 +3,15 @@ def is_anagram(test, original):
 #   use list comprehension to build a list of lowercase characters for both the test and original arguments
 #   sort the lists and then compare if they are the same
     
-#   test_list = sorted([c.lower() for c in test])
-#   original_list = sorted([c.lower() for c in original])
-#   return test_list == original_list
+        #   test_list = sorted([c.lower() for c in test])
+        #   original_list = sorted([c.lower() for c in original])
+        #   return test_list == original_list
+    
+#   learnt that the list comprehension could be omitted and instead apply sorted directly to the lowercase strings as below
+    
+        #   return sorted(test.lower()) == sorted(original.lower())
+    
+
 
 
     # refactored to a more performant solution below, counting rather than multiple comparisons/rearrangements required to sort
