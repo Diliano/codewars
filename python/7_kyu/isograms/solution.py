1ad0631a5f3b7cba2b963827caebfb33480d6971
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-02-06 20:52:52 +0000

    Refactored to a more concise solution using a set

--- python/7_kyu/isograms/solution.py
+++ python/7_kyu/isograms/solution.py
@@ -1,17 +1,23 @@
 def is_isogram(string):
-    count_dict = {}
+    # first solution below
+    # iterate through each character in the given string, converting each to lowercase
+    # if the character is found in count_dict, return False as the character is repeating
+    # otherwise add the character to count_dict with a value of 1
+    # if the loop completes without returning False, return True as the string must be an Isogram
+        # count_dict = {}
+        # for c in string:
+        #     c = c.lower()
+        #     if c in count_dict:
+        #         return False
+        #     else:
+        #         count_dict[c] = 1
+        # return True
     
-    # iterate through each character in the given string
-    for c in string:
-        # convert the character to lowercase
-        c = c.lower()
-        # if the character is found in count_dict, return False as there is a repeating character
-        if c in count_dict:
-            return False
-        else:
-            # otherwise, add the character to count_dict with a value of 1
-            count_dict[c] = 1
+
+    # refactored to create a set from the given string after being converted to lowercase
+    # the set is a collection of the unique values i.e "HiHi" would become {"h", "i"}
+    string_set = set(string.lower())
     
-    # if the above completes without returning False (by finding a character that already exists in count_dict)
-    # return True as the given string must be an Isogram
-    return True
+    # compare the length of the given string and the set
+    # if the lengths are equal, the string must be an Isogram and otherwise is not
+    return len(string) == len(string_set)
