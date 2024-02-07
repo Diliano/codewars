57628f4840a5766d396c4eebac77d8c795999865
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-02-07 22:06:38 +0000

    Refactored to a more concise solution using string count() function and sum() function

--- python/7_kyu/count_the_digit/solution.py
+++ python/7_kyu/count_the_digit/solution.py
@@ -1,17 +1,25 @@
 def nb_dig(n, d):
+    # first solution below
+    # convert digit to a string and create a list of squares (also as strings) for 0 to n (inclusive)
+    # iterate through the squares and then each character, increasing count by 1 if the character matches the target digit
+        # string_d = str(d)
+        # count = 0
+        # squares = [str(k ** 2) for k in range(0, n + 1)]
+        # for square in squares:
+        #     for c in square:
+        #         if c == string_d:
+        #             count += 1
+        # return count
+    
+
+
+    # refactored to a more concise solution
     # convert digit to a string
     string_d = str(d)
-    count = 0
     
-    # create a list of string squares for 0 to n (inclusive)
+    # create a list of squares (also as strings) for 0 to n (inclusive)
     squares = [str(k ** 2) for k in range(0, n + 1)]
     
-    # loop through each of the string squares
-    for square in squares:
-        # loop through each character in the string square
-        for c in square:
-            # if the character matches the string digit, increase count by 1
-            if c == string_d:
-                count += 1
-    
-    return count
+    # use string count function to count each target digit found in each square
+    # use sum function to aggregrate the counts into a total count
+    return sum(square.count(string_d) for square in squares)
