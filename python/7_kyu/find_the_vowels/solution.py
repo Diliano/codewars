46f9c96647055471276910173505b39cdc80a8cf
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-04-22 19:30:00 +0100

    Added comments for solution file

--- python/7_kyu/find_the_vowels/solution.py
+++ python/7_kyu/find_the_vowels/solution.py
@@ -2,8 +2,11 @@ def vowel_indices(word):
     result = []
     vowels = set("aeiouyAEIOUY")
     
+    # use enumerate to iterate through each index and character in the given word
     for index, c in enumerate(word):
+        # if the current character is a vowel
         if c in vowels:
+            # add the current index + 1 to the result, as the kata uses 1-indexing
             result.append(index + 1)
         
     return result
